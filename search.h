bd1c8914fbc49eb51eeec17c437b6945cb6228dd
mateog03 <104777599+mateog03@users.noreply.github.com>
2022-05-11 17:46:32 +0000

    simplifications

---
+++ search.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <chrono>
+#include <limits>
+#include <atomic>
+#include "board.h"
+
+constexpr int max_ply = 256;
+constexpr int mate = std::numeric_limits<int>::max();
+constexpr int min_mate = mate - max_ply;
+
+using pv_t = std::vector<move_t>;
+using tc_t = std::chrono::high_resolution_clock;
+
+struct info_t {
+	void reset();
+	void report(const pv_t& pv, int depth, int score);
+
+	tc_t::time_point start;
+	move_t prev_best;
+	int64_t nodes;
+};
+
+class searcher_t {
+public:
+	void prepare(long msec, int depth);
+	void request_stop();
+	bool should_stop(int64_t nodes);
+
+	int negamax(board_t& board, info_t& info, pv_t& pv, int alpha, int beta, int depth, int ply);
+	int quiesce(board_t& board, info_t& info, int alpha, int beta);
+	void think(board_t& board);
+
+private:
+	std::atomic<bool> stop;
+	tc_t::time_point max_time;
+	int max_depth;
+	bool infinite;
+};
